102bc33d91956d8f10e98a93c2446bf93632c5a0
Daniel Montesano <daniel.montesano@gmail.com>
2017-11-16 16:03:50 +0100

    Cambios dibujar

--- dibujar.m
+++ dibujar.m
@@ -1,39 +1,39 @@
-name = 'line2';
-v = genvarname(name);
+function [  ] = dibujar2( name )
 figure;
-if eval(['size(' v ',2)'])==3
-    
-
+if size(name,2)==3
     subplot(2,1,1);
-    plot(eval([ v '.f']),eval([ v '.S11']));
+    plot(name.f,name.S11);
     title('Magnitude (dB)');
     legend('S11');
     subplot(2,1,2);
-    plot(eval([ v '.f']),eval(['unwrap(' v '.S11_P)']));
+    plot(name.f,name.S11_P);
     title('Phase (deg)');
     legend('S11');
 
 end
 
-if eval(['size(' v ',2)'])==9
+if size(name,2)==9
     subplot(2,1,1);
     hold on;
-    plot(eval([ v '.f']),eval([ v '.S11']));
-    plot(eval([ v '.f']),eval([ v '.S12']));
-    plot(eval([ v '.f']),eval([ v '.S21']));
-    plot(eval([ v '.f']),eval([ v '.S22']));
+    plot(name.f,name.S11);
+    plot(name.f,name.S12);
+    plot(name.f,name.S21);
+    plot(name.f,name.S22);
     hold off;
     title('Magnitude (dB)');
     legend('S11', 'S12', 'S21', 'S22');
     subplot(2,1,2);
     hold on
-    plot(eval([ v '.f']),eval(['unwrap(' v '.S11_P)']));
-    plot(eval([ v '.f']),eval(['unwrap(' v '.S12_P)']));
-    plot(eval([ v '.f']),eval(['unwrap(' v '.S21_P)']));
-    plot(eval([ v '.f']),eval(['unwrap(' v '.S22_P)']));
+    plot(name.f,unwrap(name.S11_P));
+    plot(name.f,unwrap(name.S12_P));
+    plot(name.f, unwrap(name.S21_P));
+    plot(name.f,unwrap(name.S22_P));
+
     hold off;
     title('Phase (deg)');
     legend('S11', 'S12', 'S21', 'S22');
 end
+clearvars name;
 
-clearvars v name;
+end
+
