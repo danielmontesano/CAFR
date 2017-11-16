43ed451f880fb082877c794426580c685839a81f
ivaniturat <33063228+ivaniturat@users.noreply.github.com>
2017-11-17 00:10:22 +0100

    intento1

--- StoR.m
+++ StoR.m
@@ -10,7 +10,7 @@ if size(S,2)==9
     S_bi(2,1,:) = 10.^(S.S21/10).*exp(j.*deg2rad(S.S21_P));
     S_bi(2,2,:) = 10.^(S.S22/10).*exp(j.*deg2rad(S.S22_P));
     for n=1:size(S_bi,3)
-        R(1,1,n) = det(S_bi(:,:,n))./S_bi(2,1,n);
+        R(1,1,n) = -det(S_bi(:,:,n))./S_bi(2,1,n);
     end
     R(1,2,:) = S_bi(1,1,:)./S_bi(2,1,:);
     R(2,1,:) = -S_bi(2,2,:)./S_bi(2,1,:);
--- calculos.m
+++ calculos.m
@@ -1,2 +1,41 @@
+%% Ejercicio 3
+%%
 
-filter = filter.S11;
+clc
+clear
+close all
+%%
+load('matlab.mat')
+dibujar(filter)
+dibujar(line1)
+dibujar(line2)
+dibujar(reflect1)
+dibujar(reflect2)
+dibujar(through)
+
+%%
+
+
+[ S_bi_T, R_T ] = StoR(through);
+[ S_bi_L1, R_L1 ] = StoR(line1);
+
+    for n=1:size(R_T,3)
+
+        T(:,:,n) = R_L1(:,:,n)*inv(R_T(:,:,n));
+        
+        p = [T(2,1,n) T(2,2,n)-T(1,1,n) -T(1,2,n)];
+        Troot(:,n) = roots(p);
+        a_c(n)= Troot(1);
+        c_a = 1./a_c;
+        b(n)=Troot(2);
+        
+        g(n) = R_T(2,2,n);
+        d(n) = R_T(2,2,n)*R_T(1,1,n);
+        e(n) = R_T(2,2,n)*R_T(1,2,n);
+        f(n) = R_T(2,2,n)*R_T(2,1,n);
+        
+    end
+    
+r22rho22 = g.*((f-(d.*c_a))/(1-(e.*c_a)));
+alphaporA = ((d-(b.*f))/(1-(e.*c_a)));
+beta_alpha = ((e-b)/(d-(b.*f)));
