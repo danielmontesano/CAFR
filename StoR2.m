a40949e96f8be161825416f08740d1a2cc1a3469
Daniel Montesano <daniel.montesano@gmail.com>
2017-11-21 08:58:48 +0100

    Copia github

---
+++ StoR2.m
@@ -0,0 +1,21 @@
+function [ S_bi, R ] = StoR( S )
+%Pasar S (dB y ang) a S binomial. Pasa tambien S binomial a R. 
+if size(S,2)==3
+    S_bi = 10.^(S.S11/20).*exp(j.*deg2rad(S.S11_P));
+
+end
+if size(S,2)==9
+    S_bi(:,1) = 10.^(S.S11/20).*exp(j.*deg2rad(S.S11_P));
+    S_bi(:,2) = 10.^(S.S12/20).*exp(j.*deg2rad(S.S12_P));
+    S_bi(:,3) = 10.^(S.S21/20).*exp(j.*deg2rad(S.S21_P));
+    S_bi(:,4) = 10.^(S.S22/20).*exp(j.*deg2rad(S.S22_P));
+%     for n=1:size(S_bi,3)
+%         R(1,1,n) = -det(S_bi(:,:,n))./S_bi(2,1,n);
+%     end
+%     R(1,2,:) = S_bi(1,1,:)./S_bi(2,1,:);
+%     R(2,1,:) = -S_bi(2,2,:)./S_bi(2,1,:);
+%     R(2,2,:) = 1./S_bi(2,1,:);
+R = [];
+end
+end
+
